eadb338d0826a73b32b87e98391bc2358cca1fb6
DWilcox02 <58661242+DWilcox02@users.noreply.github.com>
2021-08-02 11:19:17 -0400

    ain

---
+++ 0simpleApp.py
@@ -0,0 +1,15 @@
+import justpy as jp
+
+## Create quasar page
+def app(): #returns quasar page
+    wp = jp.QuasarPage()
+    #Add elements
+    #Classes add style, spaces in between each
+    #Respect the order that elements are added it goes top to bottom
+    h1 = jp.QDiv(a = wp, text = "Analysis of course reviews", classes = "text-h3 text-center q-pa-md") 
+    #Quasar division (from HTML)
+    p1 = jp.QDiv(a = wp, text = "These grpahs represent course review analysis")
+    return wp
+
+jp.justpy(app)
+# Control C to stop the process
